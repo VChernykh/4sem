eba56ffd62ae02069d11337ee1447623ffde39ed
VChernykh <33216308+VChernykh@users.noreply.github.com>
2020-04-22 20:21:30 +0300

    Update row_count.c

--- row_count.c
+++ row_count.c
@@ -5,31 +5,33 @@
 
 int main(int argc, char *argv[])
 {
-        int rank, proc_num;
-        int size, each;
-        double total_sum = 0;
-        double start, finish = 0;
-
-        MPI_Init(&argc, &argv);
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-        MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
-
+        int my_rank;
+        int proc_num;
+        int i;
+        int len;
+        int k; //number of terms for one proc
         int* sbuf;
         int* rbuf;
-        int i;
-        if(rank == 0){
-                sscanf(argv[1], "%d", &size);
-                if((size % proc_num) == 0){
-                        each = size / proc_num;
+        double start_time, end_time = 0;
+        double result = 0;
+
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+
+        if(my_rank == 0){
+                sscanf(argv[1], "%d", &len);
+                if((len % proc_num) == 0){
+                        k = len / proc_num;
                 }
                 else{
-                        each = size / proc_num + 1;
+                        k = len / proc_num + 1;
                 }
 
-                sbuf = (int*)malloc(each * proc_num * sizeof(int));
+                sbuf = (int*)malloc(k * proc_num * sizeof(int));
 
-                for(i = 1; i <= each * proc_num; i++){
-                        if(i <= size){
+                for(i = 1; i <= k * proc_num; i++){
+                        if(i <= len){
                                 sbuf[i - 1] = i;
                         }
                         else{
@@ -42,19 +44,16 @@ int main(int argc, char *argv[])
         double* sum_buf = (double*)malloc(proc_num * sizeof(double));
         MPI_Scatter(sbuf, k, MPI_INT, rbuf, k, MPI_INT, 0, MPI_COMM_WORLD);
 
-//time measurement
         MPI_Barrier(MPI_COMM_WORLD);
         if(my_rank == 0)
                 start_time = MPI_Wtime();
 
         for(i = k - 1; i >= 0; i--){
-        //for(i = 1; i <=  k; i++){     
                 if(rbuf[i] != 0)
                         item += 6 / 3.1415926535 / 3.1415926535 / rbuf[i] / rbuf[i];
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
-//
 
         if(my_rank == 0)
                 end_time = MPI_Wtime();
