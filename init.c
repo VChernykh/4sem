3407e215f2d87f7e1ccbd56681fdf4738ab509f6
VChernykh <33216308+VChernykh@users.noreply.github.com>
2020-05-13 23:52:23 +0300

    Update init.c

--- init.c
+++ init.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
         out_file = (char*)calloc(FILE_SIZE, sizeof(char));
         FILE* in;
         FILE* out;	
-int* num1;
+	int* num1;
 	int* num2;
 	int* result;
 	
@@ -27,8 +27,7 @@ int* num1;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
 	int i;
-	if(rank == 0)
-	{
+	if(rank == 0){
 		                sscanf(argv[1], "%s", in_file);
                 in = fopen(in_file, "r");
                 sscanf(argv[2], "%s", out_file);
@@ -58,8 +57,7 @@ int* num1;
 	int* rbuf1;
 	int* rbuf2;
 	int* result0;
-	int buf = 0;
-	int worker = 0;
+	int buf, worker = 0;
 	int* cur_sol1;
 	int* cur_sol2;
 	int* is_spec;
@@ -69,8 +67,7 @@ int* num1;
 	int* my_sol2;
 	int wr;
 	MPI_Barrier(MPI_COMM_WORLD);
-	if(rank == 0)
-	{
+	if(rank == 0){
 		int already = 0;
 		cur_sol1 = (int*)malloc(number * sizeof(int));
 		cur_sol2 = (int*)malloc(number * sizeof(int));
@@ -78,19 +75,16 @@ int* num1;
 		razr1 = (int*)malloc((block_num + 1) * sizeof(int));
 		razr2 = (int*)malloc(block_num * sizeof(int));
 		start = MPI_Wtime();
-		for(i = 0; i < block_num; i++)
-		{
+		for(i = 0; i < block_num; i++){
 			MPI_Recv(&buf, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-			if(Status.MPI_TAG == 2)
-			{
+			if(Status.MPI_TAG == 2){
 				already++;
 				worker = Status.MPI_SOURCE;
 				MPI_Recv(&wr, 1, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 				is_spec[wr] = 0;
 				MPI_Recv(&cur_sol1[wr * block_size], block_size, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 				MPI_Recv(&razr1[wr], 1, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-				if(Status.MPI_TAG == 7)
-				{
+				if(Status.MPI_TAG == 7){
 					is_spec[wr] = 1;
 					MPI_Recv(&cur_sol2[wr * block_size], block_size, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 					MPI_Recv(&razr2[wr], 1, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
@@ -106,30 +100,25 @@ int* num1;
 		}
 		int* ostbuf = (int*)malloc(ost * sizeof(int));
 		ostbuf[ost - 1] = num1[block_num * block_size + ost - 1] + num2[block_size * block_num + ost - 1];
-		for(i = ost - 2; i >= 0; i--)
-		{
+		for(i = ost - 2; i >= 0; i--){
 			ostbuf[i] = num1[block_num * block_size + i] + num2[block_num * block_size + i];
-			if(ostbuf[i + 1] > MAX_NUM)
-			{
+			if(ostbuf[i + 1] > MAX_NUM){
 				ostbuf[i] += 1;
 			}
 		}
 		is_spec[block_num] = 0;
 		razr1[block_num] = 0;
-		for(i = 1; i < proc_num; i++)
-		{
+		for(i = 1; i < proc_num; i++){
 			MPI_Send(&buf, 1, MPI_INT, i, 5, MPI_COMM_WORLD);
 		}
-		for(i = 0; i < block_num - already; i++)
-		{
+		for(i = 0; i < block_num - already; i++){
 			MPI_Recv(&buf, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 			worker = Status.MPI_SOURCE;
 			MPI_Recv(&wr, 1, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 			is_spec[wr] = 0;
 			MPI_Recv(&cur_sol1[wr * block_size], block_size, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 			MPI_Recv(&razr1[wr], 1, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-			if(Status.MPI_TAG == 7)
-			{
+			if(Status.MPI_TAG == 7){
 				is_spec[wr] = 1;
 				MPI_Recv(&cur_sol2[wr * block_size], block_size, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 				MPI_Recv(&razr2[wr], 1, MPI_INT, worker, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
@@ -145,23 +134,18 @@ int* num1;
 				razr1[block_num] = 1;
 		}
 		int j;
-		for(i = block_num - 1; i >= 0; i--)
-		{
-			if(is_spec[i] == 1)
-			{
-				if((is_spec[i + 1] == 1) || (is_spec[i + 1] != 1 && razr1[i + 1] == 1))
-				{
+		for(i = block_num - 1; i >= 0; i--){
+			if(is_spec[i] == 1){
+				if((is_spec[i + 1] == 1) || (is_spec[i + 1] != 1 && razr1[i + 1] == 1)){
 					for(j = 0; j < block_size; j++)
 						result0[i * block_size + j] = cur_sol2[i * block_size + j];
 				}
-				else
-				{
+				else{
 					for(j = 0; j < block_size; j++)
 						result0[i * block_size + j] = cur_sol1[i * block_size + j];
 				}
 			}
-			else
-			{
+			else{
 				for(j = 0; j < block_size; j++)
 					result0[i * block_size + j] = cur_sol1[i * block_size + j];
 				if(is_spec[i+1] == 1)
@@ -171,22 +155,19 @@ int* num1;
 			}
 		}
 
-		for(i = 0; i < number; i++)
-		{
-			if(result0[i] < 1000000000)
+		for(i = 0; i < number; i++){
+			if(result0[i] <= MAX_NUM)
 				fprintf(out, "%d", result0[i]);
-			else
-			{
+			else{
 				if(i != 0)
-					fprintf(out, "%09d", result0[i] - 1000000000);
+					fprintf(out, "%09d", result0[i] - MAX_NUM - 1);
 				else
 					fprintf(out, "%d", result0[0]);
 			}
 		}
 
 	}
-	else
-	{
+	else{
 		int row_num;
 		rbuf1 = (int*)malloc(block_size * sizeof(int));
 		rbuf2 = (int*)malloc(block_size * sizeof(int));
@@ -195,8 +176,7 @@ int* num1;
 		int razr1;
 		int razr2;
 	
-		while(1)
-		{
+		while(1){
 			MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 			
 			MPI_Recv(&row_num, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
@@ -204,15 +184,12 @@ int* num1;
 				break;
 			MPI_Recv(rbuf1, block_size, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 			MPI_Recv(rbuf2, block_size, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-			if(Status.MPI_TAG == 1)
-			{
+			if(Status.MPI_TAG == 1){
 				razr1 = 0;
 				my_sol1[block_size - 1] = rbuf1[block_size - 1] + rbuf2[block_size - 1];
 				
-				if(my_sol1[block_size - 1] != MAX_NUM)
-				{
-					for(i = block_size - 2; i >= 0; i--)
-					{
+				if(my_sol1[block_size - 1] != MAX_NUM){
+					for(i = block_size - 2; i >= 0; i--){
 						my_sol1[i] = rbuf1[i] + rbuf2[i];
 						if(my_sol1[i+1] > MAX_NUM)
 							my_sol1[i] += 1;
@@ -224,13 +201,11 @@ int* num1;
 					MPI_Send(my_sol1, block_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
 					MPI_Send(&razr1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 				}
-				else
-				{
+				else{
 					razr1 = 0;
 					razr2 = 0;
 					my_sol2[block_size - 1] = my_sol1[block_size - 1] + 1;
-					for(i = block_size - 2; i >= 0; i--)
-					{
+					for(i = block_size - 2; i >= 0; i--){
 						my_sol1[i] = rbuf1[i] + rbuf2[i];
 						if(my_sol1[i+1] > MAX_NUM)
 							my_sol1[i] += 1;
@@ -251,16 +226,14 @@ int* num1;
 				}	
 
 			}
-			else
-			{
+			else{
 				break;
 			}
 		}
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
-	if(rank == 0)
-	{
+	if(rank == 0){
 		end = MPI_Wtime();
 		printf("%f\n", end - start); 
 		fclose(in); fclose(out);
