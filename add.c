578622bc9911068ce896aee45ccf876f55410354
VChernykh <33216308+VChernykh@users.noreply.github.com>
2020-05-13 23:26:53 +0300

    Update add.c

--- add.c
+++ add.c
@@ -1,158 +1,147 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
+#define MAX_NUM 999999999
+#define FILE_SIZE 15
 
-int main(int argc, char *argv[]){
 
-	int my_rank;
-	int proc_num;
-	int i;
-	int num_len;
-	int count;
-	double start_time = 0;
-	double end_time = 0;
-	char inpf[10];
-	char outf[10];
-	FILE* fr;
-	FILE* fw;
-	int* num1;
-	int* num2;
-	int* result;
-	int b_per_proc;
-	MPI_Status Status;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+int  main(int argc, char *argv[]){
 
-	if(my_rank == 0){
-		if(argc != 3){
-			printf("Programm requires two file names!\n");
-			MPI_Finalize();
-			return 0;
-		}
-		sscanf(argv[1], "%s", &inpf);
-		if((fr = fopen(inpf, "r")) == NULL){
-			printf("Can't open first file!\n");
-		}
-		sscanf(argv[2], "%s", &outf);
-		if((fw = fopen(outf, "w")) == NULL){
-			printf("Can't open second file!\n");
-		}
-		
-		fscanf(fr, "%d", &num_len);
-		count = num_len / 9;
+        //files
+        char* in_file;
+        char* out_file;
+        in_file = (char*)calloc(FILE_SIZE, sizeof(char));
+        out_file = (char*)calloc(FILE_SIZE, sizeof(char));
+        FILE* in;
+        FILE* out;
 
-		num1 = (int*)calloc(count, sizeof(int));
-		num2 = (int*)calloc(count, sizeof(int));
-		for(i = 0; i < count; i++){
-			fscanf(fr, "%9d", &num1[i]);
-		}
-		
-		for(i = 0; i < count; i++){
-			fscanf(fr, "%9d", &num2[i]);
-		}
-	
-		b_per_proc = count / (proc_num - 1);
-	}
-	MPI_Bcast(&b_per_proc, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	
-	int* rbuf1;
-	int* rbuf2;
-	int* result0;
-	MPI_Barrier(MPI_COMM_WORLD);
-	if(my_rank == 0){
-		start_time = MPI_Wtime();
-		for(i = 1; i < proc_num; i++){
-			MPI_Send(&num1[b_per_proc*(i-1)], b_per_proc, MPI_INT, i, 0, MPI_COMM_WORLD);
-			MPI_Send(&num2[b_per_proc*(i-1)], b_per_proc, MPI_INT, i, 0, MPI_COMM_WORLD);
-		}
-			
-		result0 = (int*)calloc(count, sizeof(int));
-		for(i = 1; i < proc_num; i++){
-			MPI_Recv(&result0[b_per_proc*(i-1)], b_per_proc, MPI_INT, i, 0, MPI_COMM_WORLD, &Status);
-		}
-		for(i = 0; i < count; i++){
-			if(result0[i] < 1000000000){
-				fprintf(fw, "%d", result0[i]);
-			}
-			else{
-				if(i != 0)
-					fprintf(fw, "%09d", result0[i] - 1000000000);
-				else
-					fprintf(fw, "%d", result0[0]);
-			}						
-		}	
+        int rank, proc_num, len, number, share;
+        double start, end  = 0;
+        int* n1;
+        int* n2;
+        int* summa;
 
-	}
-	else{
-		rbuf1 = (int*)calloc(b_per_proc, sizeof(int));
-		rbuf2 = (int*)calloc(b_per_proc, sizeof(int));
-		MPI_Recv(rbuf1, b_per_proc, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
-		MPI_Recv(rbuf2, b_per_proc, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+        MPI_Status Status;
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
 
-		result = (int*)calloc(b_per_proc, sizeof(int));
-		result[b_per_proc - 1] = rbuf1[b_per_proc - 1] + rbuf2[b_per_proc - 1];
-		if(result[b_per_proc - 1] != 999999999 && b_per_proc >= 2){
-			for(i = b_per_proc - 2; i >= 0; i--){
-				result[i] = rbuf1[i] + rbuf2[i];
-				if(result[i+1] > 999999999)
-					result[i] += 1;
-			}
-			int flag = 0;
-			if(result[0] > 999999999)
-				flag = 1;
-			if(my_rank != 1)
-				MPI_Send(&flag, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
-			int prev = 0;
-			if(my_rank != proc_num - 1)
-				MPI_Recv(&prev, 1, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, &Status);
-			result[b_per_proc - 1] += prev;
-			MPI_Send(result, b_per_proc, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		}
-		else{
-			int* resultplus = (int*)calloc(b_per_proc, sizeof(int));
-			resultplus[b_per_proc - 1] = result[b_per_proc - 1] + 1;
-			if(b_per_proc >= 2){
-				for(i = b_per_proc - 2; i >= 0; i--){
-					result[i] = rbuf1[i] + rbuf2[i];
-					if(result[i+1] > 999999999)
-						result[i] += 1;
-					resultplus[i] = rbuf1[i] + rbuf2[i];
-					if(resultplus[i+1] > 999999999)
-						resultplus[i] += 1;
-				}
-			}
-			int prev = 0;
-			if(my_rank != proc_num - 1)
-				MPI_Recv(&prev, 1, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, &Status);
-			if(prev == 0){
-				MPI_Send(result, b_per_proc, MPI_INT, 0, 0, MPI_COMM_WORLD);
-				int flag = 0;
-				if(result[0] > 999999999)
-					flag = 1;
-				if(my_rank != 1)
-					MPI_Send(&flag, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
-			}
-			else{
-				MPI_Send(resultplus, b_per_proc, MPI_INT, 0, 0, MPI_COMM_WORLD);
-				int flag = 0;
-				if(resultplus[0] > 999999999)
-					flag = 1;
-				if(my_rank != 1)
-					MPI_Send(&flag, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
-			}
-		}
 
-	}
+        int i;
+        if(rank == 0){
+                sscanf(argv[1], "%s", in_file);
+                in = fopen(in_file, "r");
+                sscanf(argv[2], "%s", out_file);
+                out = fopen(out_file, "w");
+                fscanf(in, "%d", &len);
+                number = len / 9;
+                share = number / (proc_num - 1);
+                n1 = (int*)calloc(number, sizeof(int));
+                n2 = (int*)calloc(number,  sizeof(int));
+                for(i = 0; i < number; i++)
+                        fscanf(in, "%9d", &n1[i]);
+                for(i = 0; i < number; i++)
+                        fscanf(in, "%9d", &n2[i]);
+        }
+        int* summa_init;
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	if(my_rank == 0){
-		end_time = MPI_Wtime();
-		printf("Time: %f\n", end_time - start_time); 
-		fclose(fr);
-		fclose(fw);
-	}
-	MPI_Finalize();
-	return 0;
+        MPI_Bcast(&share, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(rank == 0){
+                summa_init = (int*)calloc(number, sizeof(int));
+                start = MPI_Wtime();
+
+
+                for(i = 1; i < proc_num; i++){
+                        MPI_Send(&n1[(i - 1) * share], share, MPI_INT, i, 0, MPI_COMM_WORLD);
+                        MPI_Send(&n2[(i - 1) * share], share, MPI_INT, i, 0, MPI_COMM_WORLD);
+                }
+
+                for(i = 1; i < proc_num; i++)
+                        MPI_Recv(&summa_init[(i - 1) * share], share, MPI_INT, i, 0, MPI_COMM_WORLD, &Status);
+                for(i = 0; i < number; i++){
+                        if(summa_init[i] <= MAX_NUM)
+                                fprintf(out, "%d", summa_init[i]);
+                        else{
+                                if(i != 1)
+                                        fprintf(out, "%09d", summa_init[i] - MAX_NUM - 1);
+                                else
+                                        fprintf(out, "%d", summa_init[0]);
+                        }
+                }
+
+        }
+        else{
+                int* buf1;
+                int* buf2;
+                buf1 = (int*)calloc(share, sizeof(int));
+                buf2 = (int*)calloc(share, sizeof(int));
+                MPI_Recv(buf1, share, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+                MPI_Recv(buf2, share, MPI_INT, 0, 0, MPI_COMM_WORLD, &Status);
+
+                summa = (int*)calloc(share, sizeof(int));
+                summa[share - 1] = buf1[share - 1] + buf2[share - 1];
+                if (summa[share - 1] != MAX_NUM && share >= 2){
+                        for(i = share - 2; i >= 0; i--){
+                                summa[i] = buf1[i] + buf2[i];
+                                if(summa[i + 1] > MAX_NUM)
+                                        summa[i] += 1;
+                        }
+                        int flag, flag_pre = 0;
+                        if(summa[0] > MAX_NUM)
+                                flag = 1;
+                        if(rank != 1)
+                                MPI_Send(&flag, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
+
+                        if(rank != proc_num - 1)
+                                MPI_Recv(&flag_pre, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &Status);
+                        summa[share - 1] += flag_pre;
+                        MPI_Send(summa, share, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                }
+                else{
+                        int* summa_end = (int*)calloc(share, sizeof(int));
+                        summa_end[share - 1] = summa[share - 1] + 1;
+                        if(share >= 2){
+                                for(i = share - 2; i >= 0; i--){
+                                        summa[i] = buf1[i] + buf2[i];
+                                        if(summa[i+1] > MAX_NUM)
+                                                summa[i] += 1;
+                                        summa_end[i] = buf1[i] + buf2[i];
+                                        if(summa_end[i + 1] > MAX_NUM)
+                                                summa_end[i] += 1;
+                                }
+                        }
+                        int flag_pre = 0;
+                        if(rank != proc_num - 1)
+                                MPI_Recv(&flag_pre, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &Status);
+                        if(flag_pre == 0){
+                                MPI_Send(summa, share,  MPI_INT, 0, 0, MPI_COMM_WORLD);
+                                int flag = 0;
+                                if(summa[0] > MAX_NUM)
+                                        flag = 1;
+                                if(rank != 1)
+                                        MPI_Send(&flag, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
+                        }
+                        else{
+                                MPI_Send(summa_end, share, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                                int flag = 0;
+                                if(summa_end[0] > MAX_NUM)
+                                        flag = 1;
+                                if(rank != 1)
+                                        MPI_Send(&flag, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
+                        }
+                }
+
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(rank == 0){
+                end = MPI_Wtime();
+		printf("%f\n", end - start);
+                fclose(in); fclose(out);
+        }
+        MPI_Finalize();
+        return 0;
 }
+ 
