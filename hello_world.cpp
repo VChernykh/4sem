03e9577e40f637bdfac862a7dd8cef188f1e0423
VChernykh <33216308+VChernykh@users.noreply.github.com>
2020-12-17 19:37:26 +0300

    Update and rename hello_world.cpp to hello_world.c

--- hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include "omp.h"
-#include <iostream>
-
-int main(int argc, char** argv){
-    omp_set_num_threads(3);
-#pragma omp parallel{
-        std::cout << "Hello world!" << std::endl;
-    }
-}
